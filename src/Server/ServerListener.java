00fc470315633a033e465a0f5f2a594979f44548
Daniel Jägestedt <daniel_jege@hotmail.com>
2023-11-20 13:51:23 +0100

    Created ServerListener class to allow multiple users, changes in Server class to accommodate

---
+++ src/Server/ServerListener.java
@@ -0,0 +1,28 @@
+package Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerListener {
+
+    public ServerListener() {
+
+        try (ServerSocket ss = new ServerSocket(12345);) {
+            while (true) {
+
+                Socket s = ss.accept();
+                Server server = new Server(s);
+                server.start();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main (String[] args) {
+        ServerListener sl = new ServerListener();
+    }
+}
+
