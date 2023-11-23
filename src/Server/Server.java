ba4c803015277d5b10646e73ea6ffb815865dbe8
”Eddie <eddie.boschek@gmail.com>
2023-11-23 11:38:21 +0100

    Created properties file and implemented it in Server class.

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,18 +1,31 @@
 package Server;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Properties;
 
 public class Server extends Thread {
     Socket s;
     Category categories = new Category();
+    Properties p = new Properties();
     public Server(Socket s) {
         this.s = s;
     }
 
     public void run() {
+
+        try {
+            p.load(new FileInputStream("src/Server/QuestionsRounds.properties"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String questionsQuantity = p.getProperty("questions", "2");
+        int roundsQuantity = Integer.parseInt(p.getProperty("rounds", "2"));
+        System.out.println(questionsQuantity);
+        System.out.println(roundsQuantity);
 
         try(
             ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream());
