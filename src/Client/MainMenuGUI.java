07eab8222f3e934f3fce819c163fb509b7cbc25a
Daniel Jägestedt <daniel_jege@hotmail.com>
2023-12-01 03:27:18 +0100

    fixes

--- src/Client/MainMenuGUI.java
+++ src/Client/MainMenuGUI.java
@@ -219,7 +219,7 @@ public class MainMenuGUI {
     }
 
     public void playRound() throws IOException, ClassNotFoundException, InterruptedException {
-        if (currentRound < 6) {
+        if (currentRound < numbOfRounds) {
             System.out.println("current round: " + currentRound);
             QuizGUI quizGUI = new QuizGUI(client, currentRound, properties);
             currentRound++;
